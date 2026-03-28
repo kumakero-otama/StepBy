fa5c50259f26bc02a6d8db687bda03216cb8b23d
Maria <lynn.fukuyama@gmail.com>
2026-03-29 07:39:27 +0900

    fix: v1.1.0 - clarify PRO lock message: login = tags/photos, PRO = comments

--- UI1/config.js
+++ UI1/config.js
@@ -8,9 +8,10 @@
   globalScope.APP_CONFIG = {
     APP_BASE_PATH: typeof current.APP_BASE_PATH === "string" ? current.APP_BASE_PATH : defaults.APP_BASE_PATH,
     API_BASE_URL: typeof current.API_BASE_URL === "string" ? current.API_BASE_URL : defaults.API_BASE_URL,
-    VERSION: '1.0.9'
+    VERSION: '1.1.0'
   };
 })(window);
+
 
 
 
