4a0226bc315ae5a63295d40598f9144169487a31
kumakero-otama <kumakero.otama@gmail.com>
2026-05-04 16:26:00 +0900

    UI0: v1.18.12 ・設定画面のバージョン表示で参照されるバックエンドのバージョン番号をv1.26.0から最新のv1.27.3に更新した

--- UI0/version.js
+++ UI0/version.js
@@ -1,6 +1,6 @@
 // アプリケーションバージョン
-const FRONTEND_VERSION = "1.18.11";
-const BACKEND_VERSION = "1.26.0";
+const FRONTEND_VERSION = "1.18.12";
+const BACKEND_VERSION = "1.27.3";
 
 const VERSION_LABELS = {
   ja: {
