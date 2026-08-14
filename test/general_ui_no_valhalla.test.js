7e1f6117a5214e1566997f03fae74bb2db8eeb59
kumakero-otama <kumakero.otama@gmail.com>
2026-08-14 20:28:16 +0900

    fix: localize Japanese app bar titles

--- test/general_ui_no_valhalla.test.js
+++ test/general_ui_no_valhalla.test.js
@@ -10,4 +10,17 @@ const source = fs.readFileSync(path.join(__dirname, "../UI10/map/map.js"), "utf8
 const normalFunction = source.slice(source.indexOf("function requestSnappedLocation"), source.indexOf("if (comparisonTestButtonEl)"));
 assert.doesNotMatch(normalFunction, /\/api\/match|compareValhalla|saveFittingComparison|renderFittingComparison/);
 assert.match(normalFunction, /browserOsmMatcher\.match/);
+
+for (const [relativePath, expectedTitle] of [
+  ["profile/Index.html", "プロフィール"],
+  ["help/Index.html", "ヘルプ"],
+  ["setting/Index.html", "設定"],
+]) {
+  const html = fs.readFileSync(path.join(__dirname, "../UI10", relativePath), "utf8");
+  assert.match(
+    html,
+    new RegExp(`<h1 class="app-bar-title">${expectedTitle}</h1>`),
+    `日本語画面 ${relativePath} の上部タイトルを日本語で表示すること`,
+  );
+}
 console.log("general UI and normal fitting path have no Valhalla dependency");
